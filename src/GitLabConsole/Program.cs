baf0f1a91554186c1e2bcf8492eac3290a7e7e1e
Viktor Andersson <v.andersson@me.com>
2019-03-07 15:13:08 +0000

    Add solution and project

---
+++ src/GitLabConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitLabConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var paragraph = "There operation eye bed. Long spring power. Standard sea risk action player bring.";
+            Console.Write(paragraph);
+        }
+    }
+}
